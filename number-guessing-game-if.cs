4d72f0891fb78a27ae651f168691dd5f6d03face
OliviaRocks <130154848+OliviaRocks@users.noreply.github.com>
2024-12-11 17:37:10 +0200

    Add files via upload

---
+++ number-guessing-game-if.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+Random random = new Random();
+int randomNumber = random.Next(1, 101);
+
+Console.WriteLine("Guess a number from 1 to 100: ");
+
+bool guessedCorrectly = false;
+
+while (!guessedCorrectly)
+{
+    string inputNumber = Console.ReadLine();
+
+    if (inputNumber != null && int.TryParse(inputNumber, out int convertedNumber))
+    {
+        if (convertedNumber > randomNumber)
+        {
+            Console.WriteLine($"The number is smaller than {convertedNumber}");
+        }
+        else if (convertedNumber < randomNumber)
+        {
+            Console.WriteLine($"The number is larger than {convertedNumber}");
+        }
+        else if (convertedNumber == randomNumber)
+        {
+            Console.WriteLine($"You guessed correctly the number {convertedNumber}");
+        }
+        else
+        {
+            Console.WriteLine($"Wrong input. Please type a number between 1 and 100");
+        }
+    }
+}
